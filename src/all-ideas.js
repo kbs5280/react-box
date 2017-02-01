5d9c78bb3f58487cf25edd33e4bcf2401ecbfeab
Kris Sparks <kbs5280@gmail.com>
2017-02-01 12:55:55 -0700

    Create and render new ideas
    at last. New ideas are added to the top of the list. When the page is loaded/refreshed they render in the same order.

--- src/all-ideas.js
+++ src/all-ideas.js
@@ -4,8 +4,6 @@ import Idea from './idea';
 class AllIdeas extends React.Component {
   renderIdeas() {
 
-    console.log(this.props.ideas);
-
     return this.props.ideas.map((idea, index) =>
       <Idea key={index}
             title={idea.title}
